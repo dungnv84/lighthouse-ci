1e3ec0ab47c6539819d574b9b82910dbe1a75d04
dung.nv8 <dung.nv8>
2025-08-06 22:47:30 +0700

    1

--- lighthouserc.js
+++ lighthouserc.js
@@ -11,12 +11,12 @@ module.exports = {
     upload: {
       target: 'lhci',
       serverBaseUrl: 'http://admin:admin@lighthouse-ci.local/',
-      token: '829f9553-e766-4e69-8342-4d12662908e8',
+      token: 'b04dd69f-d04a-4b05-ba7f-0be497bd478c',
     },
   },
 };
 
-
+// 4fU4qFJH0jOTZlzBy62pgpg1RByHOsaEwE5kbJLo
 // export DOMAINS_API_URL="https://gateway.dev-kiotvietweb.fun/api/v2/merchants/domain/list-domain-devops?page=1&perPage=10"
 // export DOMAINS_API_TOKEN="Dc3GWSOBlHOEXxnzQhL0"
 // export LHCI_SERVER_URL="http://admin:admin@lighthouse-ci.local/"
