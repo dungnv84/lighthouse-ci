34cdd5cd97da43263bbdbe138ae57d9e8b2a1f38
dung.nv8 <dung.nv8@kiotviet.com>
2025-08-07 09:02:41 +0700

    1

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,8 +8,8 @@ module.exports = {
   },
 };
 
-// 4fU4qFJH0jOTZlzBy62pgpg1RByHOsaEwE5kbJLo
+// o26KZpmqfEoklUlY76r02uMIsmbEHiHsqvlqBgQk
 // export DOMAINS_API_URL="https://gateway.dev-kiotvietweb.fun/api/v2/merchants/domain/list-domain-devops?page=1&perPage=10"
 // export DOMAINS_API_TOKEN="Dc3GWSOBlHOEXxnzQhL0"
 // export LHCI_SERVER_URL="http://admin:admin@lighthouse-ci.local/"
-// export LHCI_TOKEN="b3741a45-7ba9-4ba4-a105-2b23b766209e"
+// export LHCI_TOKEN="e8b54a64-b64f-4214-9a97-81709d61cd43"
